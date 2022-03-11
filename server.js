0a43f73fa57c4760fa76db3a9eae8dccd3f7e96c
Rene Rodriguez <rennyjr.id@gmail.com>
2022-03-10 18:33:21 -0700

    Modify server path

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, "/dist/ascii-img")));
+app.use(express.static(path.join("./dist/ascii-img")));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "/dist/ascii-img/index.html"));
+  res.sendFile(path.join("./dist/ascii-img/index.html"));
 });
 app.listen(process.env.PORT || 8080);
